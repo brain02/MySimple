b356793281bd8fadf5ef2c8d77f628a73ee02978
kovolenkoi <kovalenkoi@bsindustry.ru>
2022-03-15 22:43:21 +0300

    unit test Mockito

--- domain/src/test/java/qqq/qqq/domain/usecase/GetDataUserUseCaseTest.kt
+++ domain/src/test/java/qqq/qqq/domain/usecase/GetDataUserUseCaseTest.kt
@@ -2,29 +2,35 @@ package qqq.qqq.domain.usecase
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
 import qqq.qqq.domain.models.UserData
 import qqq.qqq.domain.repository.UserRepository
 
 
-class TestRepository : UserRepository {
-    override fun getUser(): UserData {
-        return UserData(firstName = "Ivan", lastName = "Kovalenko")
-    }
-
-    override fun saveUser(userData: UserData): Boolean {
-        TODO("Not yet implemented")
-    }
-
-}
+//class TestRepository : UserRepository {
+//    override fun getUser(): UserData {
+//        return UserData(firstName = "Ivan", lastName = "Kovalenko")
+//    }
+//
+//    override fun saveUser(userData: UserData): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
 
 class GetDataUserUseCaseTest {
 
+    val userRepository = mock<UserRepository>()
+
     @Test
     fun test() {
-        val testRepository = TestRepository()
-        val useCase = GetDataUserUseCase(userRepository = testRepository)
-        val result = useCase.execute()
+        val testUserData = UserData(firstName = "Ivan", lastName = "Kovalenko")
+        Mockito.`when`(userRepository.getUser()).thenReturn(testUserData)
+
+        val useCase = GetDataUserUseCase(userRepository = userRepository)
+        val actual = useCase.execute()
         val expected = UserData(firstName = "Ivan", lastName = "Kovalenko")
-        assertEquals(expected, result)
+        assertEquals(expected, actual)
     }
 }
