5dc890026a9a6bc612486eda031d906e56134a98
kovolenkoi <kovalenkoi@bsindustry.ru>
2022-03-21 11:45:17 +0300

    Binding Adapter

--- app/src/main/java/qqq/qqq/adapter/MyRecycleAdapter.kt
+++ app/src/main/java/qqq/qqq/adapter/MyRecycleAdapter.kt
@@ -2,36 +2,32 @@ package qqq.qqq.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import qqq.qqq.R
+import qqq.qqq.databinding.AdapterViewBinding
 
 class MyRecycleAdapter : RecyclerView.Adapter<MyRecycleAdapter.MyViewHolder>() {
 
     private lateinit var context: Context
     private var arrayList: ArrayList<String> = arrayListOf()
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvName: TextView = itemView.findViewById(R.id.tvName)
-        val btnCLick: Button = itemView.findViewById(R.id.btnClick)
-    }
+    class MyViewHolder(val binding: AdapterViewBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.adapter_view, parent, false)
         context = parent.context
-        return MyViewHolder(itemView)
+        val binding = AdapterViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding = binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val itemData = arrayList[position]
-        holder.tvName.text = itemData
-        holder.btnCLick.setOnClickListener {
-            Toast.makeText(context, itemData, Toast.LENGTH_LONG).show()
+
+        with(holder.binding){
+            tvName.text = itemData
+            btnClick.setOnClickListener {
+                Toast.makeText(context, itemData, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
